30ed36401e120faadefe6aa330ce3aea959459cc
Corentin Larroque <co.larroque@gmail.com>
2018-11-16 15:35:42 +0100

    New iOS project

---
+++ ios/RNVoxeetConferencekit/RNVoxeetConferencekit.h
@@ -0,0 +1,21 @@
+//
+//  RNVoxeetConferencekit.h
+//  RNVoxeetConferencekit
+//
+//  Created by Corentin Larroque on 11/16/18.
+//  Copyright © 2018 Voxeet. All rights reserved.
+//
+
+//#import <Foundation/Foundation.h>
+
+#if __has_include("RCTBridgeModule.h")
+#import "RCTBridgeModule.h"
+#else
+#import <React/RCTBridgeModule.h>
+#endif
+
+@interface RNVoxeetConferencekit : NSObject <RCTBridgeModule>
+    
+- (void)test;
+
+@end
---
+++ ios/RNVoxeetConferencekit/RNVoxeetConferencekit.m
@@ -0,0 +1,37 @@
+//
+//  RNVoxeetConferencekit.m
+//  RNVoxeetConferencekit
+//
+//  Created by Corentin Larroque on 11/16/18.
+//  Copyright © 2018 Voxeet. All rights reserved.
+//
+
+#import "RNVoxeetConferencekit.h"
+
+@implementation RNVoxeetConferencekit
+
+    RCT_EXPORT_MODULE();
+    
+//    RCT_EXPORT_METHOD(test)
+//    {
+////        [VoxeetModule initialize:consumerKey consumerSecret:consumerSecret];
+//        NSLog(@"BONJOUR");
+//
+//        [VoxeetModule connect:externalId name:name avatarURL:avatarURL completion:^{
+//            resolve(@"");
+//        } onError:^(NSString *error) {
+//            reject(@"connect_error", error, nil);
+//        }];
+//    }
+    
+    RCT_EXPORT_METHOD(test)
+    {
+//        [VoxeetModule appearMaximized:enabled];
+        NSLog(@"BONJOUR");
+    }
+    
+//    - (void)test {
+//        NSLog(@"BONJOUR");
+//    }
+    
+@end
