bbc412c4fc3b041b18ff1262671f433dff412bb7
Kévin Le Perf <codlabtech@gmail.com>
2021-04-18 01:20:41 +0200

    explicit zip path

--- patch.react.aar.sh
+++ patch.react.aar.sh
@@ -4,9 +4,9 @@ PATH=`ls node_modules/react-native/android/com/facebook/react/react-native/ | gr
 
 ZIP=node_modules/react-native/android/com/facebook/react/react-native/$PATH/react-native-$PATH.aar
 
-zip -d $ZIP jni/x86_64/libc++_shared.so
-zip -d $ZIP jni/x86/libc++_shared.so
-zip -d $ZIP jni/armeabi-v7a/libc++_shared.so
-zip -d $ZIP jni/arm64-v8a/libc++_shared.so
+/usr/bin/zip -d $ZIP jni/x86_64/libc++_shared.so
+/usr/bin/zip -d $ZIP jni/x86/libc++_shared.so
+/usr/bin/zip -d $ZIP jni/armeabi-v7a/libc++_shared.so
+/usr/bin/zip -d $ZIP jni/arm64-v8a/libc++_shared.so
 
 echo "Patch done for the outdated libc++ inside React (r21b embedded insides Voxeet's dependencies"
