8d90de0c2f51936a8fec801bc9c129d3a645658e
Kévin Le Perf <codlabtech@gmail.com>
2021-04-18 13:55:45 +0200

    fix issue with UI not displayed

--- android/src/main/java/com/voxeet/rn/manifests/RNVoxeetManifestComponent.java
+++ android/src/main/java/com/voxeet/rn/manifests/RNVoxeetManifestComponent.java
@@ -53,8 +53,10 @@ public final class RNVoxeetManifestComponent extends AbstractManifestComponentPr
         RNVoxeetManifestComponent.root_view_provider = new RNRootViewProvider(application, VoxeetToolkit.instance());
         application.registerActivityLifecycleCallbacks(RNVoxeetManifestComponent.root_view_provider);
         VoxeetToolkit.instance().setProvider(RNVoxeetManifestComponent.root_view_provider);
+        RNVoxeetManifestComponent.root_view_provider.registerLifecycleListener(VoxeetToolkit.instance());
 
         VoxeetToolkit.instance().enableOverlay(true);
+        VoxeetToolkit.instance().enable(VoxeetToolkit.instance().getConferenceToolkit());
 
         //change the overlay used by default
         VoxeetToolkit.instance().getConferenceToolkit().setDefaultOverlayState(OverlayState.EXPANDED);
